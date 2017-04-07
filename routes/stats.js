cfa0a418062961c5ec04d0ab7019a7510a5d37eb
cristian <crist.1707@hotmail.com>
2017-04-07 03:12:15 -0500

    si

--- routes/stats.js
+++ routes/stats.js
@@ -11,8 +11,6 @@ var config = {
   password: 'cris0717', //env var: PGPASSWORD
   host: 'localhost', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 
