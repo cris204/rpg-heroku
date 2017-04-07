0e1c5b40ce372e7cb8e2be13423767216187f16e
cristian <crist.1707@hotmail.com>
2017-04-07 01:48:33 -0500

    finish

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { head: "RPG",
                         title: 'Servicios',
-                        Unlock: "desbloquear un personaje: http://localhost:3000/unlock/(id del jugador)?character_id=(id del character)",
-                        Stats: "modificar los stats de un character: http://localhost:3000/stats/(id del jugador)?character_id=(id del character)&statToIncrease=(stat a subir)",
-                        Team : "ver o modificar el team: http://localhost:3000/team/(id del jugador)?c1=(id del character)&c2=(id del character)&c3=(id del character) puedes cambiar un solo character o los 3 al mismo tiempo",
-                        Character : "mirar los stats bases de un character: http://localhost:3000/character/(id del character) ",
-                        Battle: 'batalla, saber ganador y perdedor:  http://localhost:3000/battle/(id primer jugador - id segundo jugador)?score1=(score del jugador 1 en esta partida)&score2=(score del jugador 2 en esta partida) los ids van separados por un "-"'});
+                        Unlock: "desbloquear un personaje: https://rpg-master-op.herokuapp.com/(id del jugador)?character_id=(id del character)",
+                        Stats: "modificar los stats de un character: https://rpg-master-op.herokuapp.com/(id del jugador)?character_id=(id del character)&statToIncrease=(stat a subir)",
+                        Team : "ver o modificar el team: https://rpg-master-op.herokuapp.com/(id del jugador)?c1=(id del character)&c2=(id del character)&c3=(id del character) puedes cambiar un solo character o los 3 al mismo tiempo",
+                        Character : "mirar los stats bases de un character: https://rpg-master-op.herokuapp.com/(id del character) ",
+                        Battle: 'batalla, saber ganador y perdedor:  https://rpg-master-op.herokuapp.com/(id primer jugador - id segundo jugador)?score1=(score del jugador 1 en esta partida)&score2=(score del jugador 2 en esta partida) los ids van separados por un "-"'});
 });
 
 module.exports = router;
