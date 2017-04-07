7e757611a20b6e38bb35f12684f822ecf4e583fd
cristian <crist.1707@hotmail.com>
2017-04-07 01:59:55 -0500

    final?

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { head: "RPG",
                         title: 'Servicios',
+                        id: "los user id son 10 y 11, los character id son del 1 al 5 estan desbloqueados los 3 primeros"
                         Unlock: "desbloquear un personaje: https://rpg-master-op.herokuapp.com/(id del jugador)?character_id=(id del character)",
                         Stats: "modificar los stats de un character: https://rpg-master-op.herokuapp.com/(id del jugador)?character_id=(id del character)&statToIncrease=(stat a subir)",
                         Team : "ver o modificar el team: https://rpg-master-op.herokuapp.com/(id del jugador)?c1=(id del character)&c2=(id del character)&c3=(id del character) puedes cambiar un solo character o los 3 al mismo tiempo",
